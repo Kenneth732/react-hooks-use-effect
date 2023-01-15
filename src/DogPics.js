1b89b75bc0bf0c9868a12d31a98c2b22daa5e64b
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-01-15 18:17:44 +0300

    useEffect

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,29 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
+
 function DogPics() {
-  const [images, setImages] = useState([]);
+    const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then((r) => r.json())
-      .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
-  });
-
-  console.log("render");
-
-  return (
-    <div>
-      {images.map((image) => (
-        <img src={image} key={image} />
-      ))}
-    </div>
-  );
+    useEffect(() =>{
+        fetch("https://dog.ceo/api/breeds/image/random/3")
+        .then((res) => res.json())
+        .then((data) =>{
+            setImages(data.message);
+        });
+    }, []);
+    return(
+        <div>
+            {images.map((image) =>{
+                <img src={image} key={image} />
+            })}
+        </div>
+    );
 }
 
-export default DogPics;
+export default DogPics
